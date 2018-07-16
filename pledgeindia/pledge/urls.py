9f0a549370fb557d4e66befcc790e4a5cfda5a16
Ayush Agarwal <thisisayushaa@gmail.com>
2018-07-16 08:11:37 +0530

    (add) HomeTemplate, URLPatterns, Views, Templates.

--- pledgeindia/pledge/urls.py
+++ pledgeindia/pledge/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-
+from .views import *
 # urlpatterns
 
-urlpatterns = []
+app_name = "pledge"
+
+urlpatterns = [
+    path("", HomeView, name="home"),
+]
--- pledgeindia/pledge/views.py
+++ pledgeindia/pledge/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def HomeView(request):
+    return render(request, "pledge/index.html")
